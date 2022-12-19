83bec769e7d014ae677ec81b9c1f47c7a8f5d9ac
Gyeongtae Min <prierkt@gmail.com>
2022-12-19 19:45:22 +0900

    how use 'useEffect'

--- 0_whyReact/begin-react-app/src/App.js
+++ 0_whyReact/begin-react-app/src/App.js
@@ -1,14 +1,37 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [searchWord, setSearchWord] = useState("");
+
   const onClickHandler = () => {
     setCounter((prev) => prev + 1);
   };
+
+  const onChangeHandler = (event) => setSearchWord(event.target.value);
+  console.log("I run all the time.");
+
+  const iRunOnlyOnce = () => {
+    console.log("I run only once.");
+  };
+
+  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("SEARCH FOR", searchWord);
+  }, [searchWord]);
+  useEffect(() => {
+    console.log("I run when both change.");
+  }, [counter, searchWord]);
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Search here."
+        value={searchWord}
+        onChange={onChangeHandler}
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button text="Click me" onClick={onClickHandler} />
     </div>
